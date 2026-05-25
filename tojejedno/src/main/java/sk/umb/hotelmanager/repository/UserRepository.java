f698018fa5989fd72916fe0feb73b3ac9b7b0aae
tgrznar <tgrznar@student.umb.sk>
2026-05-25 19:07:43 +0200

    fix: update repository layer with custom query methods

--- tojejedno/src/main/java/sk/umb/hotelmanager/repository/UserRepository.java
+++ tojejedno/src/main/java/sk/umb/hotelmanager/repository/UserRepository.java
@@ -6,5 +6,7 @@ import sk.umb.hotelmanager.entity.User;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findbyEmail(String email);
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByGoogleId(String googleId);
 }
