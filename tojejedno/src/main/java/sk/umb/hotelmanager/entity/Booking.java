b8688fb146cb7958562e0a6b82662822f32c00b8
tgrznar <tgrznar@student.umb.sk>
2026-05-25 18:54:17 +0200

    fix: update Booking entity with missing fields and annotations

--- tojejedno/src/main/java/sk/umb/hotelmanager/entity/Booking.java
+++ tojejedno/src/main/java/sk/umb/hotelmanager/entity/Booking.java
@@ -1,44 +1,56 @@
 package sk.umb.hotelmanager.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import sk.umb.hotelmanager.enums.BookingStatus;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "bookings")
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Booking {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "from_date")
+    @Column(name = "from_date", nullable = false)
     private LocalDate fromDate;
 
-    @Column(name = "to_date")
+    @Column(name = "to_date", nullable = false)
     private LocalDate toDate;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private BookingStatus status;
 
-    @Column(name = "created_at")
+    @Column(name = "created_at",updatable = false)
     private LocalDateTime createdAt;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
+    @Column(name = "guest_name", nullable = false)
+    private String guestName;
+
     @ManyToOne
     @JoinColumn(name = "room_id")
     private Room room;
 
-    public Booking() {
-        this.createdAt = LocalDateTime.now();
+    @Column(name = "total_price", nullable = false)
+    private BigDecimal totalPrice;
+
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = LocalDateTime.now();
     }
 }
