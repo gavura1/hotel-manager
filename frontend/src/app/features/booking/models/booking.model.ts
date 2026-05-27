b820d438fa49f840bf66d64d0fc971d8eeae91dd
tgrznar <tgrznar@student.umb.sk>
2026-05-27 18:55:51 +0200

    feat: add Booking model

---
+++ frontend/src/app/features/booking/models/booking.model.ts
@@ -0,0 +1,11 @@
+export interface Booking {
+  id: number;
+  roomId: number;
+  fromDate: Date;
+  toDate: Date;
+  guestName: string;
+  totalPrice: number;
+  status: 'CONFIRMED' | 'CANCELLED' | 'COMPLETED';
+  createdAt: Date;
+  note: string;
+}
